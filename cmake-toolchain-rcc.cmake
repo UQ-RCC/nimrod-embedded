28cac13067afa99ccd96e6862a2ab998096c1d50
Zane van Iperen <z.vaniperen@uq.edu.au>
2019-06-06 19:01:10 +1000

    cleanup toolchain file

--- cmake-toolchain-rcc.cmake
+++ cmake-toolchain-rcc.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
 set(tuple x86_64-centos7-linux-gnu)
-set(tools /gpfs1/sw7/RCC/NimrodG/devenv/x86_64-centos7-linux-gnu)
+set(tools /gpfs1/sw7/RCC/NimrodG/devenv/${tuple})
 
 set(CMAKE_C_COMPILER ${tools}/bin/${tuple}-gcc)
 set(CMAKE_CXX_COMPILER ${tools}/bin/${tuple}-g++)
@@ -9,5 +9,3 @@ set(CMAKE_CXX_COMPILER ${tools}/bin/${tuple}-g++)
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -static-libstdc++ -mindirect-branch=thunk -I/usr/include" CACHE STRING "" FORCE)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static-libstdc++ -mindirect-branch=thunk -I/usr/include" CACHE STRING "" FORCE)
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static-libstdc++ -Wl,-rpath-link,/lib64" CACHE STRING "" FORCE)
-
-set(PBS_ROOT "/opt/pbs" CACHE STRING "" FORCE)
